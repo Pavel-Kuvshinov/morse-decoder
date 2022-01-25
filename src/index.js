9f62c8c22794188ba4ba98a1f65b3f4e0de25e36
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2022-01-25 23:34:39 +0300

    feat: end task morse-decoder
    End task morse-decoder. All tests pasted.

--- src/index.js
+++ src/index.js
@@ -37,8 +37,77 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+
+    // My first and very old decision))
+
+    /*
+
+    let items = ['00', '11', '10', '**'];
+
+    let memoryStr = "";
+    let memoryArr = [];
+    let resultArr = [];
+    let result = "";
+
+    let strArr = expr.match(/.{1,2}/g);
+
+    for (let i = 0; i<=strArr.length; i++){
+        
+        if (i%5==0 && i>0){
+            memoryArr.push(memoryStr);
+            memoryStr = "";
+        }
+        
+        let item = items.indexOf(strArr[i]);
+        if (item == 0){
+            continue;
+        } else if (item == 1){
+            memoryStr += "-";
+            continue;
+        } else if (item == 2){
+            memoryStr += ".";
+            continue;
+        } else if (item == 3){
+            memoryStr += " ";
+            i = i + 4;
+            continue;
+        }
+        
+    }
+
+    for (let j=0; j<memoryArr.length; j++){
+        if (MORSE_TABLE.hasOwnProperty(memoryArr[j]) === true){
+        resultArr[j] = MORSE_TABLE[memoryArr[j]];
+        } else if (memoryArr[j] === " "){
+            resultArr[j] = memoryArr[j];
+        }
+    }
+
+    result = resultArr.join('');
+
+    return result;
+
+    */
+
+    // My second decision;
+
+    let items = ['00', '11', '10', '**********'];
+    let strArr = expr.match(/.{1,10}/g);
+    
+    let resultArr = strArr.map(item => {
+      if (item == items[3]) return ' ';
+      let memorySymbol = item.match(/.{1,2}/g).map(elem =>{
+        if (elem == items[0]) return '';
+        if (elem == items[1]) return '-';
+        if (elem == items[2]) return '.'
+        }).join('');
+      return MORSE_TABLE[memorySymbol];
+    });
+    
+    return resultArr.join('');
+
 }
 
 module.exports = {
